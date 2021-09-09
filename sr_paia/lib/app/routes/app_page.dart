a16f1df44843ffaff42fb1bfef48dc4840cadb46
natan.santos <natanvs@gmail.com>
2021-09-09 14:22:46 -0300

    add pages

--- sr_paia/lib/app/routes/app_page.dart
+++ sr_paia/lib/app/routes/app_page.dart
@@ -1,1 +1,15 @@
 
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:sr_paia/app/routes/app_routes.dart';
+import 'package:sr_paia/app/ui/home/home_bindings.dart';
+import 'package:sr_paia/app/ui/home/home_page.dart';
+import 'package:sr_paia/app/ui/login/login_bindings.dart';
+import 'package:sr_paia/app/ui/login/login_page.dart';
+
+class AppPages {
+  static final pages = [
+    GetPage(name: Routes.INITIAL, page: () => LoginPage(), binding: LoginBinding()),
+    GetPage(name: Routes.LOGIN, page: () => LoginPage(), binding: LoginBinding()),
+    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding())
+  ];
+}
